010cfdeec651c4e96d5390b8d936105ff1973c53
shubhammmgithub <shubhamrajbr28@gmail.com>
2026-03-06 14:43:51 +0530

    "Final layout of the portfolio"

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#element', {
+  strings: [
+    'Generative AI Specialist.', 
+    'AI/ML Intern.', 
+    'MLOps Engineer.', 
+    'Python Developer.'
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
